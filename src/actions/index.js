ba8467c03c563e7e51cdcaca334b78dab1e15251
Lucas <lucas@agtlucas.com>
2016-06-19 09:37:03 -0300

    Add actions/index.js

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export function action (type, payload) {
+  if (typeof payload === 'undefined') {
+    return { type }
+  }
+  return { type, payload }
+}
+
+function createAction (type) {
+  return payload => action(type, payload)
+}
+
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const toggleTodo = createAction(TOGGLE_TODO)
